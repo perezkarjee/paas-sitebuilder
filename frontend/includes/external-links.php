dcde1f905a12c543ad07f380827df713c4bb74b2
perez karjee <pcx1256@gmail.com>
2014-10-10 16:20:53 -0700

    frontend2
    frontend2

---
+++ frontend/includes/external-links.php
@@ -0,0 +1,21 @@
+<?php
+/* external links start */
+
+
+//core
+$blog ='http://www.';
+$forum ='http://www.';
+$tutorials ='http://www.';
+
+//social
+$facebook ='http://www.';
+$twitter ='http://www.';
+$googleplus ='http://www.';
+$youtube ='http://www.';
+
+//other
+$link ='http://www.';
+$link2 ='http://www.';
+
+/* external links end */
+?>
