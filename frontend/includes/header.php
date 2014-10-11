5446279234fef7265206cd1c1b598528a51a7b83
perez karjee <pcx1256@gmail.com>
2014-10-10 19:09:26 -0700

    css
    css

--- frontend/includes/header.php
+++ frontend/includes/header.php
@@ -12,7 +12,7 @@
 <title><?php echo $title; ?></title>
 <meta name="description" content="">
 <meta name="viewport" content="width=device-width, initial-scale=1">
-<link rel="stylesheet" href="../css/main.css">
+<link rel="stylesheet" href="css/main.css">
 <link rel="stylesheet" href="../css/magnific-popup.css">
 <script src="../js/vendor/custom.modernizr.js"></script>
 </head>
