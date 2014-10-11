b4e8f81abda78b7889bdfac196fdcfc06b144d96
perez karjee <pcx1256@gmail.com>
2014-10-11 15:53:03 -0700

    test
    test

--- frontend/index.php
+++ frontend/index.php
@@ -6,18 +6,20 @@
 //head
 $title = 'Title';
 include 'includes/external-links.php';
-include 'includes/header.php';
+include 'includes/header2.php';
 include 'includes/menu.php';
 ?>
 
 <?php
 //slider
+include 'includes/header-2.php';
 include 'sliders/slider-home.php';
 ?>
 
 <?php
 //container
 include 'containers/container-home.php';
+include 'containers/container-testimonial.php';
 ?>
 
 <?php
---
+++ frontend/sliders/slide-r-home.php
@@ -0,0 +1,20 @@
+<?php
+//container start 
+?>
+
+
+<section id="main" role="main">
+  <section id="hero">
+    <div class="row">
+      <div class="large-10 large-centered column text-center">
+        <div id="hero-message" class="fadeInDown animated">
+          <h1>Powerful Hosting Solution</h1>
+          <h4>The more affordable approach to stable web hosting solutions. Inexpensive, reliable, and feature-rich!</h4>
+          <a href="signup.html" class="button">SIGN UP FOR A PLAN</a></div>
+      </div>
+    </div>
+  </section>
+
+<?php
+//container end
+?>
