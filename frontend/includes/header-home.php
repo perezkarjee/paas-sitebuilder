fb739b6de780112e6c5095745713e2de0f5c4987
perez karjee <pcx1256@gmail.com>
2014-10-11 16:24:48 -0700

    slider

--- frontend/includes/header-home.php
+++ frontend/includes/header-home.php
@@ -18,18 +18,15 @@
 <link rel="stylesheet" href="css/main.css">
 <link rel="stylesheet" href="css/magnific-popup.css">
 
-<link rel="stylesheet" href="css/bootstrap.css" />
-	<link rel="stylesheet" href="css/bootstrap-responsive.css" />
 	<link rel="stylesheet" href="css/font-awesome.min.css" />
 	<link rel="stylesheet" href="css/font-awesome-ie7.min.css" />
 	<link rel="stylesheet" type="text/css" href="css/flexslider.css" />
-	<link rel="stylesheet" href="css/styles.css" />
+	<link rel="stylesheet" href="css/slide-testi.css" />
 			<link rel="stylesheet" href="css/slide.css" />
 
 
 <script src="js/vendor/custom.modernizr.js"></script>
 <script type="text/javascript" src="js/jquery-1.8.2.min.js"></script>
-	<script type="text/javascript" src="js/bootstrap.min.js"></script>
 	<script type="text/javascript" src="js/webfont.js"></script>
 	<script type="text/javascript" src="js/jquery.sequence-min.js"></script>
 	<script type="text/javascript" src="js/jquery.flexslider-min.js"></script>
