09d225f710acbe4418927c148a8cd439b5c577a9
perez karjee <pcx1256@gmail.com>
2014-10-11 16:34:19 -0700

    etst

--- frontend/includes/header-home.php
+++ frontend/includes/header-home.php
@@ -1,17 +1,31 @@
 <?php
 //header start
 ?>
+<!--[if lt IE 7]><html class="no-js lt-ie9 lt-ie8 lt-ie7"><![endif]-->
+<!--[if IE 7]><html class="no-js lt-ie9 lt-ie8"><![endif]-->
+<!--[if IE 8]><html class="no-js lt-ie9"><![endif]-->
+<!--[if gt IE 8]><!--><html class="no-js"><!--<![endif]-->
 
+<head>
+<meta charset="UTF-8">
+<!--[if IE]><meta http-equiv="X-UA-Compatible" content="IE=edge"><![endif]-->
+<meta name="description" content="">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta name="description" content="">
 
-	<link rel="stylesheet" href="css/bootstrap.css" />
+<link rel="stylesheet" href="css/main.css">
+<link rel="stylesheet" href="css/magnific-popup.css">
+
+	<!--link rel="stylesheet" href="css/bootstrap.css" />
 	<link rel="stylesheet" href="css/bootstrap-responsive.css" />
 	<link rel="stylesheet" href="css/font-awesome.min.css" />
 	<link rel="stylesheet" href="css/font-awesome-ie7.min.css" />
 	<link rel="stylesheet" type="text/css" href="css/flexslider.css" />
 	<link rel="stylesheet" href="css/slide-testi.css" />
-			<link rel="stylesheet" href="css/slide.css" />
+			<link rel="stylesheet" href="css/slide.css" /-->
 
 
+<script src="js/vendor/custom.modernizr.js"></script>
 <script type="text/javascript" src="js/jquery-1.8.2.min.js"></script>
 	<script type="text/javascript" src="js/bootstrap.min.js"></script>
 	<script type="text/javascript" src="js/webfont.js"></script>
