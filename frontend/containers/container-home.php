e84b284a20c47a55055c8713a06c4efa6f2087bc
perez karjee <pcx1256@gmail.com>
2014-10-11 14:34:26 -0700

    home
    home

--- frontend/containers/container-home.php
+++ frontend/containers/container-home.php
@@ -54,42 +54,42 @@
             <ul class="pricing-table">
               <li class="title">Free</li>
               <li class="price">$0 <span>/ month</span></li>
-              <li class="bullet-item"><strong>1</strong> Website</li>
-              <li class="bullet-item"><strong>1</strong> Subdomain</li>
+              <li class="bullet-item"><strong>Instant</strong> Setup</li>
+              <li class="bullet-item"><strong>Free</strong> Subdomain</li>
               <li class="bullet-item"><strong>Free</strong> Templates</li>
               <li class="bullet-item"><strong>300MB</strong> Space</li>
-              <li class="bullet-item"><strong>5</strong> Users</li>
+              <li class="bullet-item"><strong>Fully</strong> Customizable</li>
               <li class="bullet-item"><strong>Shopping Cart</strong> No</li>
-              <li class="bullet-item">cPanel &amp; FTP</li>
-              <li class="cta-button"><a class="button" href="signup.html">Get Started</a></li>
+              <li class="bullet-item">Free Support</li>
+              <li class="cta-button"><a class="button" href="signup.php">Sign Up</a></li>
             </ul>
           </div>
           <div class="large-4 column">
             <ul class="pricing-table highlight">
               <li class="title">Premium</li>
               <li class="price">$5 <span>/ month</span></li>
-              <li class="bullet-item"><strong>3 </strong> Websites</li>
-              <li class="bullet-item"><strong>Custom Domain</strong> + Subdomain</li>
+              <li class="bullet-item"><strong>Instant</strong> Setup</li>
+              <li class="bullet-item"><strong>Custom Domain</strong> + Free Subdomain</li>
               <li class="bullet-item"><strong>Premium</strong> + Free Templates</li>
               <li class="bullet-item"><strong>5000MB</strong> Space</li>
-              <li class="bullet-item"><strong>40</strong> Users</li>
-              <li class="bullet-item"><strong>Shopping Cart</strong> No</li>
-              <li class="bullet-item">cPanel &amp; SFTP</li>
-              <li class="cta-button"><a class="button" href="signup.html">Get Started</a></li>
+              <li class="bullet-item"><strong>Fully</strong> Users</li>
+              <li class="bullet-item"><strong>Shopping Cart</strong> Customizable</li>
+              <li class="bullet-item">Free Support</li>
+              <li class="cta-button"><a class="button" href="signup.php">Sign Up</a></li>
             </ul>
           </div>
           <div class="large-4 column">
             <ul class="pricing-table">
               <li class="title">Professional</li>
               <li class="price">$20 <span>/ month</span></li>
-              <li class="bullet-item"><strong>10</strong> Websites</li>
-              <li class="bullet-item"><strong>Custom Domain</strong> + Subdomain</li>
+              <li class="bullet-item"><strong>Instant</strong> Setup</li>
+              <li class="bullet-item"><strong>Custom Domain</strong> + Free Subdomain</li>
               <li class="bullet-item"><strong>Premium</strong> + Free Templates</li>
               <li class="bullet-item"><strong>Unlimited</strong> Space</li>
-              <li class="bullet-item"><strong>Unlimited</strong> Users</li>
+              <li class="bullet-item"><strong>Fully</strong> Customizable</li>
               <li class="bullet-item"><strong>Shopping Cart</strong> Yes</li>
-              <li class="bullet-item">cPanel &amp; SFTP</li>
-              <li class="cta-button"><a class="button" href="signup.html">Get Started</a></li>
+              <li class="bullet-item">Free Support</li>
+              <li class="cta-button"><a class="button" href="signup.php">Sign Up</a></li>
             </ul>
           </div>
       </div>
