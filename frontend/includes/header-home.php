46f8b5cfb0523c03044d6c6f0f4e0134342fbbf9
perez karjee <pcx1256@gmail.com>
2014-10-11 16:29:06 -0700

    slider

--- frontend/includes/header-home.php
+++ frontend/includes/header-home.php
@@ -1,22 +1,7 @@
 <?php
 //header start
 ?>
-<!--[if lt IE 7]><html class="no-js lt-ie9 lt-ie8 lt-ie7"><![endif]-->
-<!--[if IE 7]><html class="no-js lt-ie9 lt-ie8"><![endif]-->
-<!--[if IE 8]><html class="no-js lt-ie9"><![endif]-->
-<!--[if gt IE 8]><!--><html class="no-js"><!--<![endif]-->
 
-<head>
-<meta charset="UTF-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
-	<!--[if lt IE 9]><script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script><![endif]-->
-<!--[if IE]><meta http-equiv="X-UA-Compatible" content="IE=edge"><![endif]-->
-<title><?php echo $title; ?></title>
-
-<meta name="description" content="">
-<meta name="viewport" content="width=device-width, initial-scale=1">
-<link rel="stylesheet" href="css/main.css">
-<link rel="stylesheet" href="css/magnific-popup.css">
 
 	<link rel="stylesheet" href="css/bootstrap.css" />
 	<link rel="stylesheet" href="css/bootstrap-responsive.css" />
@@ -27,7 +12,6 @@
 			<link rel="stylesheet" href="css/slide.css" />
 
 
-<script src="js/vendor/custom.modernizr.js"></script>
 <script type="text/javascript" src="js/jquery-1.8.2.min.js"></script>
 	<script type="text/javascript" src="js/bootstrap.min.js"></script>
 	<script type="text/javascript" src="js/webfont.js"></script>
