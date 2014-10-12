6b9d2d7a4f01e02532ad0f4fabe043e7e51b9709
perez karjee <pcx1256@gmail.com>
2014-10-11 19:44:02 -0700

    test

--- frontend/sliders/slider-home.php
+++ frontend/sliders/slider-home.php
@@ -4,13 +4,10 @@
 <head>
 
 <link rel="stylesheet" href="css/slide.css" />
-	<link rel="stylesheet" href="css/bootstrap-responsive.css" />
 	<link rel="stylesheet" href="css/font-awesome.min.css" />
 	<link rel="stylesheet" href="css/font-awesome-ie7.min.css" />
 	<link rel="stylesheet" href="css/slider.css" />
 
-
-	<script type="text/javascript" src="js/bootstrap.min.js"></script>
 	<script type="text/javascript" src="js/webfont.js"></script>
 	<script type="text/javascript" src="js/jquery.sequence-min.js"></script>
 
