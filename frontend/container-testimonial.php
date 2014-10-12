15353bd710e33989d7362b6599f3d4922176e988
perez karjee <pcx1256@gmail.com>
2014-10-11 17:48:18 -0700

    test

---
+++ frontend/container-testimonial.php
@@ -0,0 +1,101 @@
+<?php
+//start
+?>
+
+<div class="home-testi">
+
+			<script type="text/javascript">
+
+				$(window).load(function() {
+
+				  $('.flexslider').flexslider({
+
+				    animation: "slide",
+
+				    controlNav: false,
+
+				    slideshow: false,
+
+				    prevText: "",
+
+				    nextText: ""
+
+				  });
+
+				});
+
+			</script>
+
+				<div class="flexslider">
+
+				    <ul class="slides">
+
+					    <li>
+
+							<div class="container">
+
+								<h2 class="testi-2">What our customer say </h2>
+
+								<blockquote class="testi">
+
+									<p class="testi-2">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam scelerisque faucibus risus non iaculis. Fusce a augue ante, pellentesque pretium erat. Fusce in turpis in velit tempor pretium. Integer a leo libero.</p>
+
+								</blockquote>
+
+								<a href="#"><img src="images/face1.png" alt=""></a>
+
+								<a href="#">John Doe</a>
+
+								<span>Deverloper at pixelgeeklab</span>
+
+							</div>
+
+						</li>
+
+						<li>
+
+							<div class="container">
+
+								<h2 class="testi-2">What our customer say </h2>
+
+								<blockquote class="testi">
+
+									<p class="testi-2">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam scelerisque faucibus risus non iaculis. Fusce a augue ante, pellentesque pretium erat. Fusce in turpis in velit tempor pretium. Integer a leo libero.</p>
+
+								</blockquote>
+
+								<a href="#"><img src="images/face1.png" alt=""></a>
+
+								<a href="#">John Doe</a>
+
+								<span>Deverloper at pixelgeeklab</span>
+
+							</div>
+
+						</li>
+
+					</ul>
+
+					<ul class="flex-direction-nav">
+
+						<li>
+
+							<a class="flex-prev pgl" href="#"><i class="icon-chevron-left"></i></a>
+
+						</li>
+
+						<li>
+
+							<a class="flex-next pgl" href="#"><i class="icon-chevron-right"></i></a>
+
+						</li>
+
+					</ul>
+
+				</div>
+
+		</div>
+		
+<?php
+//end
+?>
