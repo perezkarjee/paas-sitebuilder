32a6899f5eefc86d163fcad175dabca73177e537
perez karjee <pcx1256@gmail.com>
2014-10-11 19:22:33 -0700

    test

--- frontend/index.php
+++ frontend/index.php
@@ -13,12 +13,13 @@ include 'includes/menu.php';
 <?php
 //slider
 include 'sliders/home.php';
+include 'sliders/slider-home-.php';
 ?>
 
 <?php
 //container
 include 'containers/container-home.php';
-include 'containers/container-testimonial-.php';
+include 'containers/container-testimonial.php';
 ?>
 
 <?php
