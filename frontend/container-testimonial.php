ff7e72c0e5298bef5ea9c219be5ffa3a370c7131
perez karjee <pcx1256@gmail.com>
2014-10-11 17:50:13 -0700

    test
    test

--- frontend/container-testimonial.php
+++ frontend/container-testimonial.php
@@ -1,3 +1,27 @@
+<head>
+<meta charset="UTF-8">
+<!--[if IE]><meta http-equiv="X-UA-Compatible" content="IE=edge"><![endif]-->
+<meta name="description" content="">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta name="description" content="">
+
+<!--link rel="stylesheet" href="css/main.css">
+<link rel="stylesheet" href="css/magnific-popup.css"!-->
+
+	<link rel="stylesheet" href="css/bootstrap.css" />
+	<link rel="stylesheet" href="css/bootstrap-responsive.css" />
+	<link rel="stylesheet" href="css/font-awesome.min.css" />
+	<link rel="stylesheet" href="css/font-awesome-ie7.min.css" />
+	<link rel="stylesheet" type="text/css" href="css/flexslider.css" />
+	<link rel="stylesheet" href="css/testimonial.css" />
+
+
+<!--script src="js/vendor/custom.modernizr.js"></script-->
+<script type="text/javascript" src="js/jquery-1.8.2.min.js"></script>
+	<script type="text/javascript" src="js/jquery-1.8.2.min.js"></script>
+	<script type="text/javascript" src="js/webfont.js"></script>
+	<script type="text/javascript" src="js/jquery.flexslider-min.js"></script>
+</head>
 <?php
 //start
 ?>
