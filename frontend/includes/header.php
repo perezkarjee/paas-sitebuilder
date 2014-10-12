945c9b88f4247777ad01fa634c6412efe31d68f7
perez karjee <pcx1256@gmail.com>
2014-10-11 21:21:54 -0700

    ok

--- frontend/includes/header.php
+++ frontend/includes/header.php
@@ -8,8 +8,7 @@
 
 <head>
 <meta charset="UTF-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
-	<!--[if lt IE 9]><script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script><![endif]-->
+<meta name="viewport" content="width=device-width, initial-scale=1">
 <!--[if IE]><meta http-equiv="X-UA-Compatible" content="IE=edge"><![endif]-->
 <title><?php echo $title; ?></title>
 
