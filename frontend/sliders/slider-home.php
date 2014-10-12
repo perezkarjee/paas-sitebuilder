d4feefaee377bf68280654f7284d71b2462bf18f
perez karjee <pcx1256@gmail.com>
2014-10-11 19:12:40 -0700

    css

--- frontend/sliders/slider-home.php
+++ frontend/sliders/slider-home.php
@@ -4,6 +4,19 @@
 
 
 <link rel="stylesheet" href="css/slide.css" />
+<link rel="stylesheet" href="css/bootstrap.css" />
+	<link rel="stylesheet" href="css/bootstrap-responsive.css" />
+	<link rel="stylesheet" href="css/font-awesome.min.css" />
+	<link rel="stylesheet" href="css/font-awesome-ie7.min.css" />
+	<link rel="stylesheet" href="css/slider.css" />
+
+
+	<script src="js/jquery-1.8.2.min.js"></script>
+	<script type="text/javascript" src="js/bootstrap.min.js"></script>
+	<script type="text/javascript" src="js/webfont.js"></script>
+	<script type="text/javascript" src="js/jquery.sequence-min.js"></script>
+
+
 <script type="text/javascript"> 
 	    $(document).ready(function(){
 		    var options = {
