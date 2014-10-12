26366b29ed824dda3362855ba0a17d182b4f1ab5
perez karjee <pcx1256@gmail.com>
2014-10-11 18:31:06 -0700

    test
    test

--- frontend/index.php
+++ frontend/index.php
@@ -5,24 +5,23 @@
 <?php
 //head
 $title = 'Title';
-include 'includes/external-links.php';
+include 'includes/external-links-.php';
 include 'includes/header-home.php';
-include 'includes/-menu.php';
+include 'includes/menu-.php';
 ?>
 
 <?php
 //slider
-include 'includes/heade-r-home.php';
-include 'sliders/slider-home.php';
+include 'sliders/slider-home-.php';
 ?>
 
 <?php
 //container
-include 'containers/containerhome.php';
+include 'containers/container-home-.php';
 include 'container-testimonial.php';
 ?>
 
 <?php
 //foot
-include 'includes/footer.php';
+include 'includes/footer-.php';
 ?>
