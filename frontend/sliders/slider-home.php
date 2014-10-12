811e05389e4a70c66845d537ce9a6f1da6c7e90f
perez karjee <pcx1256@gmail.com>
2014-10-11 19:53:55 -0700

    test

--- frontend/sliders/slider-home.php
+++ frontend/sliders/slider-home.php
@@ -3,25 +3,18 @@
 ?>
 <head>
 
+<link rel="stylesheet" href="css/slide.css" />
 <link rel="stylesheet" href="css/bootstrap.css" />
 	<link rel="stylesheet" href="css/bootstrap-responsive.css" />
 	<link rel="stylesheet" href="css/font-awesome.min.css" />
 	<link rel="stylesheet" href="css/font-awesome-ie7.min.css" />
-	<link rel="stylesheet" href="css/slide.css" />
-	<link rel="stylesheet" type="text/css" href="css/flexslider.css" />
-
-
-	<link rel="stylesheet" href="css/styles.css" />
-	<link rel="stylesheet" type="text/css" href="css/ddsmoothmenu.css" />
-	<link rel="stylesheet" href="css/styles2.css" />
+	<link rel="stylesheet" href="css/slider.css" />
 
 
 	<script src="js/jquery-1.8.2.min.js"></script>
 	<script type="text/javascript" src="js/bootstrap.min.js"></script>
-	<script type="text/javascript" src="js/ddsmoothmenu.js"></script>
 	<script type="text/javascript" src="js/webfont.js"></script>
 	<script type="text/javascript" src="js/jquery.sequence-min.js"></script>
-	<script type="text/javascript" src="js/jquery.flexslider-min.js"></script>
 
 </head>
 <body>
@@ -53,7 +46,6 @@
 						<img class="model" src="images/slide/2.jpg" alt="slide 2" />
 					</li>
 					<li>
-						<img class="model" src="images/slide/3.jpg" alt="slide 3" />
 						<img class="model" src="images/slide/3.jpg" alt="slide 3" />
 					</li>
 				</ul>
