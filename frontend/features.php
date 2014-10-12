d00d7a648f63d539d09453254292f3dfea1a1261
perez karjee <pcx1256@gmail.com>
2014-10-12 16:12:26 -0700

    ok

---
+++ frontend/features.php
@@ -0,0 +1,30 @@
+<?php
+//frontend home start
+
+/*
+dependency--- includes/external-links.php
+dependency--- includes/header.php
+dependency--- includes/menu.php
+dependency--- includes/footer.php
+dependency--- containers/container-features.php
+dependency--- add dependency here
+*/
+?>
+
+<?php
+//head
+$title = 'Title';
+include 'includes/external-links.php';
+include 'includes/header.php';
+include 'includes/menu.php';
+?>
+
+<?php
+//container
+include 'containers/container-features.php';
+?>
+
+<?php
+//foot
+include 'includes/footer.php';
+?>
---
+++ frontend/page-temp.php
@@ -0,0 +1,29 @@
+<?php
+//frontend home start
+
+/*
+dependency--- includes/external-links.php
+dependency--- includes/header.php
+dependency--- includes/menu.php
+dependency--- includes/footer.php
+dependency--- add dependency here
+*/
+?>
+
+<?php
+//head
+$title = 'Title';
+include 'includes/external-links.php';
+include 'includes/header.php';
+include 'includes/menu.php';
+?>
+
+<?php
+//container
+include 'containers/container-.php';
+?>
+
+<?php
+//foot
+include 'includes/footer.php';
+?>
