45e623d0f749081f63304ad63d58d2c0aadf8566
perez karjee <pcx1256@gmail.com>
2014-10-11 21:13:49 -0700

    ok

--- frontend/includes/menu.php
+++ frontend/includes/menu.php
@@ -13,11 +13,14 @@
         </ul>
         <section class="top-bar-section">
           <ul class="right">
-            <li><a href="plans.html">Item1</a></li>
-            <li><a href="features.html">Item2</a></li>
-            <li><a href="about.html">Item3</a></li>
-            <li><a href="support.html">Item4</a></li>
-            <li><a href="login.html">Item5</a></li>
+            <li><a href="plans.html">Home</a></li>
+            <li><a href="features.html">System</a></li>
+			<ul>
+			<li><a href="features.html">System</a></li>
+			<ul>
+            <li><a href="about.html">About</a></li>
+            <li><a href="support.html">Contact</a></li>
+            <li><a href="login.html">Login</a></li>
             <li id="signup"><a href="signup.php" class="button no-margin">SIGN UP</a></li>
           </ul>
         </section>
