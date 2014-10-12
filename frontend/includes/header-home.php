f7a67da4c546d61364451e8f18914428d6931415
perez karjee <pcx1256@gmail.com>
2014-10-11 18:20:50 -0700

    test

--- frontend/includes/header-home.php
+++ frontend/includes/header-home.php
@@ -13,8 +13,8 @@
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <meta name="description" content="">
 
-<link rel="stylesheet" href="css/main.css">
-<link rel="stylesheet" href="css/magnific-popup.css">
+<!--link rel="stylesheet" href="css/main.css">
+<link rel="stylesheet" href="css/magnific-popup.css"-->
 
 	<link rel="stylesheet" href="css/bootstrap.css" />
 	<link rel="stylesheet" href="css/bootstrap-responsive.css" />
