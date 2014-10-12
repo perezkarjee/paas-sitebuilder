37118c040f75d87fb1dbc363b8d4e6a7356dd9d3
perez karjee <pcx1256@gmail.com>
2014-10-11 21:06:59 -0700

    ok

--- frontend/containers/container-testimonial.php
+++ frontend/containers/container-testimonial.php
@@ -45,11 +45,12 @@
 
 								<h2>What our customer say </h2>
 
-								<blockquote>
 
-									<p style="font-size:0.9em;">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam scelerisque faucibus risus non iaculis. <br>Fusce a augue ante, pellentesque pretium erat. Fusce in turpis in velit tempor pretium. Integer a leo libero.</p>
 
-								</blockquote>
+									<p style="font-size:0.9em;">"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam scelerisque faucibus risus non iaculis. 
+									<br>Fusce a augue ante, pellentesque pretium erat. Fusce in turpis in velit tempor pretium. Integer a leo libero."</p>
+
+
 
 								<a href="#"><img src="images/face1.png" alt=""></a>
 
@@ -70,7 +71,7 @@
 								<blockquote class="testi">
 
 									<p class="testi-2">Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
-									<br>Nam scelerisque faucibus risus non iaculis. Fusce a augue ante, pellentesque pretium erat. Fusce in turpis in velit tempor pretium. Integer a leo libero.</p>
+									<>Nam scelerisque faucibus risus non iaculis. Fusce a augue ante, pellentesque pretium erat. Fusce in turpis in velit tempor pretium. Integer a leo libero.</p>
 
 								</blockquote>
 
