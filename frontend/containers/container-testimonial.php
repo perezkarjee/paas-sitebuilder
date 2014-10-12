0abe0abdbb729ef46e8a698bda54c3632df80f53
perez karjee <pcx1256@gmail.com>
2014-10-11 19:08:46 -0700

    test

--- frontend/containers/container-testimonial.php
+++ frontend/containers/container-testimonial.php
@@ -2,6 +2,20 @@
 //start
 ?>
 
+<link rel="stylesheet" href="css/bootstrap.css" />
+	<link rel="stylesheet" href="css/bootstrap-responsive.css" />
+	<link rel="stylesheet" href="css/font-awesome.min.css" />
+	<link rel="stylesheet" href="css/font-awesome-ie7.min.css" />
+	<link rel="stylesheet" type="text/css" href="css/flexslider.css" />
+	<link rel="stylesheet" href="css/slider.css" />
+	<link rel="stylesheet" href="css/testimonial.css" />
+	
+	<script type="text/javascript" src="js/jquery-1.8.2.min.js"></script>
+	<script type="text/javascript" src="js/jquery-1.8.2.min.js"></script>
+	<script type="text/javascript" src="js/webfont.js"></script>
+	<script type="text/javascript" src="js/jquery.sequence-min.js"></script>
+	<script type="text/javascript" src="js/jquery.flexslider-min.js"></script>
+
 <div class="home-testi">
 
 			<script type="text/javascript">
--- frontend/includes/header-home.php
+++ frontend/includes/header-home.php
@@ -13,24 +13,12 @@
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <meta name="description" content="">
 
-<!--link rel="stylesheet" href="css/main.css">
-<link rel="stylesheet" href="css/magnific-popup.css"-->
-
-	<link rel="stylesheet" href="css/bootstrap.css" />
-	<link rel="stylesheet" href="css/bootstrap-responsive.css" />
-	<link rel="stylesheet" href="css/font-awesome.min.css" />
-	<link rel="stylesheet" href="css/font-awesome-ie7.min.css" />
-	<link rel="stylesheet" type="text/css" href="css/flexslider.css" />
-	<link rel="stylesheet" href="css/slider.css" />
-	<link rel="stylesheet" href="css/testimonial.css" />
+<link rel="stylesheet" href="css/main.css">
+<link rel="stylesheet" href="css/magnific-popup.css">
 
 
-<!--script src="js/vendor/custom.modernizr.js"></script-->
-<script type="text/javascript" src="js/jquery-1.8.2.min.js"></script>
-	<script type="text/javascript" src="js/jquery-1.8.2.min.js"></script>
-	<script type="text/javascript" src="js/webfont.js"></script>
-	<script type="text/javascript" src="js/jquery.sequence-min.js"></script>
-	<script type="text/javascript" src="js/jquery.flexslider-min.js"></script>
+
+<script src="js/vendor/custom.modernizr.js"></script>
 </head>
 
 <?php
