65a3f9a38cd200cdd8768a8cf0a6fd710726e101
perez karjee <pcx1256@gmail.com>
2014-10-13 13:28:41 -0700

    status

--- frontend/containers/container-status.php
+++ frontend/containers/container-status.php
@@ -14,73 +14,18 @@
   <section class="part">
     <div class="row title">
       <div class="large-12 column">
-        <h2>Terms & Conditions</h2>
+        <h2>System Status</h2>
       </div>
     </div>
     <div class="row">
       <div class="large-12 column">
         <p>Nullam id dolor id nibh ultricies vehicula ut id elit. Donec ullamcorper nulla non metus auctor fringilla. Donec ullamcorper nulla non metus auctor fringilla. Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Curabitur blandit tempus porttitor. Vestibulum id ligula porta felis euismod semper.</p>
         <p>Aenean eu leo quam. Pellentesque ornare sem lacinia quam venenatis vestibulum. Nulla vitae elit libero, a pharetra augue. Vestibulum id ligula porta felis euismod semper. Maecenas sed diam eget risus varius blandit sit amet non magna. Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Praesent commodo cursus magna, vel scelerisque nisl consectetur et.</p>
-        <img src="images/demo/about.jpg" alt=""></div>
-    </div>
-  </section>
-  
-  <section class="part">
-    <div class="row title">
-      <div class="large-12 column">
-        <h4>Acceptable Usage Policy</h4>
-      </div>
-    </div>
-    <div class="row">
-      <div class="large-12 column">
-        <p>Nullam id dolor id nibh ultricies vehicula ut id elit. Donec ullamcorper nulla non metus auctor fringilla. Donec ullamcorper nulla non metus auctor fringilla. Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Curabitur blandit tempus porttitor. Vestibulum id ligula porta felis euismod semper.</p>
-        <p>Aenean eu leo quam. Pellentesque ornare sem lacinia quam venenatis vestibulum. Nulla vitae elit libero, a pharetra augue. Vestibulum id ligula porta felis euismod semper. Maecenas sed diam eget risus varius blandit sit amet non magna. Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Praesent commodo cursus magna, vel scelerisque nisl consectetur et.</p>
-        <img src="images/demo/about.jpg" alt=""></div>
     </div>
   </section>
   
   
-  <section class="part">
-    <div class="row title">
-      <div class="large-12 column">
-        <h4>Privacy Policy</h4>
-      </div>
-    </div>
-    <div class="row">
-      <div class="large-12 column">
-        <p>Nullam id dolor id nibh ultricies vehicula ut id elit. Donec ullamcorper nulla non metus auctor fringilla. Donec ullamcorper nulla non metus auctor fringilla. Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Curabitur blandit tempus porttitor. Vestibulum id ligula porta felis euismod semper.</p>
-        <p>Aenean eu leo quam. Pellentesque ornare sem lacinia quam venenatis vestibulum. Nulla vitae elit libero, a pharetra augue. Vestibulum id ligula porta felis euismod semper. Maecenas sed diam eget risus varius blandit sit amet non magna. Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Praesent commodo cursus magna, vel scelerisque nisl consectetur et.</p>
-        <img src="images/demo/about.jpg" alt=""></div>
-    </div>
-  </section>
-  
-   <section class="part">
-    <div class="row title">
-      <div class="large-12 column">
-        <h4>Billing, Payments & Returns</h4>
-      </div>
-    </div>
-    <div class="row">
-      <div class="large-12 column">
-        <p>Nullam id dolor id nibh ultricies vehicula ut id elit. Donec ullamcorper nulla non metus auctor fringilla. Donec ullamcorper nulla non metus auctor fringilla. Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Curabitur blandit tempus porttitor. Vestibulum id ligula porta felis euismod semper.</p>
-        <p>Aenean eu leo quam. Pellentesque ornare sem lacinia quam venenatis vestibulum. Nulla vitae elit libero, a pharetra augue. Vestibulum id ligula porta felis euismod semper. Maecenas sed diam eget risus varius blandit sit amet non magna. Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Praesent commodo cursus magna, vel scelerisque nisl consectetur et.</p>
-        <img src="images/demo/about.jpg" alt=""></div>
-    </div>
-  </section>
-  
-  <section class="part">
-    <div class="row title">
-      <div class="large-12 column">
-        <h4>Acceptance/Agreement, Violation And Changes To Our Terms & Conditions</h4>
-      </div>
-    </div>
-    <div class="row">
-      <div class="large-12 column">
-        <p>Nullam id dolor id nibh ultricies vehicula ut id elit. Donec ullamcorper nulla non metus auctor fringilla. Donec ullamcorper nulla non metus auctor fringilla. Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Curabitur blandit tempus porttitor. Vestibulum id ligula porta felis euismod semper.</p>
-        <p>Aenean eu leo quam. Pellentesque ornare sem lacinia quam venenatis vestibulum. Nulla vitae elit libero, a pharetra augue. Vestibulum id ligula porta felis euismod semper. Maecenas sed diam eget risus varius blandit sit amet non magna. Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Praesent commodo cursus magna, vel scelerisque nisl consectetur et.</p>
-        <img src="images/demo/about.jpg" alt=""></div>
-    </div>
-  </section>
+ 
 </section>
 
 <a id="back-top" href="#"><i class="icon-caret-up"></i></a>
