e17ff7fd01952ab3fd3d481b38d2eda160d2e665
perez karjee <pcx1256@gmail.com>
2014-10-14 17:49:56 -0700

    Update index.php

--- viewer/index.php
+++ viewer/index.php
@@ -1,3 +1,22 @@
 <?php
 //viewer server
+//add dependency here
+?>
+
+<?php
+//get reruested url and store it in a variable $url
+?>
+
+<?php
+//search for the url on the database
+$url
+?>
+
+<?php
+//if found , grab the codes from the database
+//else redirect it to 404 not found page
+?>
+
+<?php
+//if codes grabbed, generate a HTML page
 ?>
